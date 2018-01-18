46e7e03768021f32a00e45f5d34e259c32923a2b
Graham Geary <graham.geary@itm-consulting.de>
2018-01-18 16:46:30 +0100

    readded removed security group attribute.

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "ec2_linux_instance" {
     ami                    = "${data.aws_ami.coreos.id}"
     instance_type          = "${var.instance_size}"
     vpc_security_group_ids = ["${aws_security_group.RuleGroupLBHttpIn.id}"]
+    security_groups        = "${var.instance_sg_id}"
     subnet_id              = "${var.private_sn_a}"
     key_name               = "${var.instance_key_name}"
     user_data              = "${replace(data.template_file.userdata.rendered,"/\\r/","")}"
