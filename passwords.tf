969b9cd81fb29e02ee4fe456bdf26a5280e53052
Malte Brodersen <malte.brodersen@zoi.de>
2018-03-12 20:11:13 +0100

    fix length function

--- passwords.tf
+++ passwords.tf
@@ -6,6 +6,6 @@ resource "random_string" "concourse_password" {
 }
 
 locals {
-  postgres_password = "${length(var.postgres_password == 0 ? random_string.postgres_password.result : var.postgres_password}"
-  concourse_password = "${length(var.concourse_password == 0 ? random_string.concourse_password.result : var.concourse_password}"
+  postgres_password = "${length(var.postgres_password) == 0 ? random_string.postgres_password.result : var.postgres_password}"
+  concourse_password = "${length(var.concourse_password) == 0 ? random_string.concourse_password.result : var.concourse_password}"
 }
