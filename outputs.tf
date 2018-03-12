cd1071513ab8e1b35c193b5baa18d019120efa3c
Malte Brodersen <malte.brodersen@zoi.de>
2018-03-12 20:19:19 +0100

    replace linux by docker

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "public_ip" {
   value = "${aws_instance.ec2_docker_instance.public_ip}"
 }
 output "instance_id" {
-  value = "${aws_instance.ec2_linux_instance.id}"
+  value = "${aws_instance.ec2_docker_instance.id}"
 }
 output "postgres_password" {
   value = "${local.postgres_password}"
